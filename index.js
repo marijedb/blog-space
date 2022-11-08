0450ee1acc21b9a85c2d75bf19d764cc0c6cd9bd
marijedb <marijedb@live.nl>
2022-11-08 18:54:00 +0100

    added new post functionality

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
+let postsArray = [];
+const form = document.getElementById('form');
+
 fetch("https://jsonplaceholder.typicode.com/posts")
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (let post of postsArr) {
-            html += `
-                <h3 class="post-title">${post.title}</h3>
-                <p>${post.body}</p>
-                <hr>
-            `
-        }
-        document.getElementById("blog-list").innerHTML = html
+        postsArray = data.slice(0, 5);
+        renderPosts()
     })
+
+
+function renderPosts() {
+    let html = "";
+    for (let post of postsArray) {
+        html += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr>
+        `
+    }
+    document.getElementById("blog-list").innerHTML = html;
+}
+
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault()
+    const postTitle = document.getElementById("new-post-title").value;
+    const postBody = document.getElementById("new-post-body").value;
+    const newPost = {
+        title: postTitle,
+        body: postBody
+    };
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: "POST",
+            body: JSON.stringify(newPost),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            postsArray.unshift(data)
+            renderPosts()
+            form.reset()
+        })
+
+})
